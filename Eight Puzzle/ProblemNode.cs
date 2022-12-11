d39f3ceffa9f5feea41654f62dc49c619f31da1a
AmirSina Mashayekh <pajuhesh80@outlook.com>
2022-12-11 16:02:42 +0330

    Added ProblemNode class

---
+++ Eight Puzzle/ProblemNode.cs
@@ -0,0 +1,18 @@
+﻿namespace Eight_Puzzle
+{
+    internal class ProblemNode
+    {
+        public PuzzleBoard State { get; }
+
+        public ProblemNode? Parent { get; }
+
+        public int PathCost { get; }
+
+        public ProblemNode(PuzzleBoard state, ProblemNode? parent, int pathCost)
+        {
+            State = state;
+            Parent = parent;
+            PathCost = pathCost;
+        }
+    }
+}
